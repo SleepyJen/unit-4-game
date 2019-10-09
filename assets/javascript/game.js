e30c3657185bf7c4a83db7c98d55e95a5d9408b8
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-08 21:29:40 -0700

    added functions for the click start button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,41 @@
-
 $(document).ready(function () {
+
+    var toggle = true;
+    var ruby, redDiamond, diamond, tearDrop;
+    var total = 0;
+
+    var startButton = $('.btn');
+    var randNum = $('#number-chosen');
+    var r = $('#ruby');
+    var chosenText = $('#chosen');
+
+    startButton.on('click', function () {
+        if (toggle) {
+            var randGenerator = Math.floor(Math.random() * 100 + 1);
+            ruby = Math.floor(Math.random() * 10 + 1);
+            redDiamond = Math.floor(Math.random() * 10 + 1);
+            diamond = Math.floor(Math.random() * 10 + 1);
+            tearDrop = Math.floor(Math.random() * 10 + 1);
+            randNum.text(randGenerator);
+
+            startButton.text("Click to Reset");
+            toggle = false;
+        } else {
+            startButton.text("Click to Start");
+            toggle = true;
+        }
+
+        if (toggle) {
+            randNum.text("0");
+            total = 0;
+            chosenText.text("Your number: ");
+        }
+    });
+
+    r.on('click', function () {
+        total += ruby
+        chosenText.text("Your number: " + total);
+    });
+
 
 });
