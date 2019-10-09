8a8c027cd91f1f7c0b6d694e39088e7c302f0b26
SleepyJen <jennifer.ni0924@gmail.com>
2019-10-09 09:44:45 -0700

    made the jewels clickable with random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
 
     var toggle = true;
-    var ruby, redDiamond, diamond, tearDrop;
+    var ruby, redDiamond, diamond, tearDrop, randGenerator;
     var total = 0;
 
     var startButton = $('.btn');
     var randNum = $('#number-chosen');
     var r = $('#ruby');
+    var rD = $('#redDiamond');
+    var d = $('#diamond');
+    var tD = $('#tearDrop');
+    var endGame = $('.endGame');
     var chosenText = $('#chosen');
 
     startButton.on('click', function () {
         if (toggle) {
-            var randGenerator = Math.floor(Math.random() * 100 + 1);
+            randGenerator = Math.floor(Math.random() * 100 + 1);
             ruby = Math.floor(Math.random() * 10 + 1);
             redDiamond = Math.floor(Math.random() * 10 + 1);
             diamond = Math.floor(Math.random() * 10 + 1);
@@ -29,13 +33,42 @@ $(document).ready(function () {
             randNum.text("0");
             total = 0;
             chosenText.text("Your number: ");
+            endGame.empty();
         }
     });
 
     r.on('click', function () {
-        total += ruby
+        total += ruby;
         chosenText.text("Your number: " + total);
+        check();
     });
 
+    rD.on('click', function () {
+        total += redDiamond;
+        chosenText.text("Your number " + total);
+        check();
+    });
 
+    d.on('click', function () {
+        total += diamond;
+        chosenText.text("Your number " + total);
+        check();
+    });
+
+    tD.on('click', function () {
+        total += tearDrop;
+        chosenText.text("Your number " + total);
+        check();
+    });
+
+    function check() {
+        if (total > randGenerator) {
+            endGame.text("Sorry!! you lost >< Try again!!");
+            while (!toggle) {
+                r.off('click');
+            }
+        } else if (total === randGenerator) {
+            endGame.text("Congrats!! you win!! Click restart to restart the Game.");
+        }
+    }
 });
